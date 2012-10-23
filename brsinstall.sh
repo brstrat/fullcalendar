3c13784e7af9ffd6cae83fcff2a611cbaf1e570a
Jason Myers <j.myers@brstrat.com>
2012-10-23 09:43:53 -0400

    brsinstall tweaks

--- brsinstall.sh
+++ brsinstall.sh
@@ -5,7 +5,7 @@ cd dist
 zip_path=$(ls | grep fullcalendar.*zip)
 unzip $zip_path
 cd $(find -maxdepth 1 -type d -name "fullcalendar*")/fullcalendar
-compiled_file="fullcalendar.min.js"
+compiled_file="fullcalendar.js"
 echo "copying $compiled_file to $1/source/static/js/libs"
-cp fullcalendar.min.js $1/source/static/js/libs/
+cp $compiled_file $1/source/static/js/libs/
 
